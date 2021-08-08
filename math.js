69a47b2ef7229cf627130f5fccf70fb8a3919fdf
iuSaiba <intiseryusrasaiba@gmail.com>
2021-08-08 13:03:50 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,15 @@
+// plus
+function  add(number1, number2){
+    const sum = number1 + number2;
+    return sum;
+}
+const ffnum = add(63, 85);
+// console.log(firstnum);
+// maltiply
+function  maltiply(number1, number2){
+    const min = number1 - number2;
+    return min;
+}
+const ffnum = maltiply(63, 85);
+console.log(ffnum);
+// maltiply
